59c884dcb26a94474ba618c5c21da1fc3d7d7f4b
LeHoangLong <le.hoang.long@outlook.com>
2019-12-09 00:31:08 +0800

    Add content to location.html

--- src/location/urls.py
+++ src/location/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<location_name>', views.location_view)
+    path('<location_name>/<test>', views.location_view)
 ]
--- src/location/views.py
+++ src/location/views.py
@@ -2,5 +2,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def location_view(request, location_name, *args, **kwargs):
-    return HttpResponse('<h1>Hello world {}<h1>'.format(location_name))
+def location_view(request, location_name, test, *args, **kwargs):
+    model = {
+        'goal_list': ['meet up', 'flirt', 'say bye bye'] 
+    }
+    return render(request, 'location.html', model)
